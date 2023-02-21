03d1aa6ee4091f5987256bdce638f8cd0bf97f10
Matthew Doged <madoged@gmail.com>
2023-02-21 13:18:55 -0500

    Added argparse to client

--- echo-client.py
+++ echo-client.py
@@ -1,15 +1,24 @@
 # echo-client.py
 
 import socket
+import codecs
+import argparse
 
 #Next step is to pass the host listening IP and port
 
-HOST = "127.0.0.1"
+HOST = ""
 PORT = 65432
+
+parser = argparse.ArgumentParser()
+parser.add_argument("Host")
+args = parser.parse_args()
+
+target = Path(args.HOST)
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST,PORT))
     s.sendall(b"Hello, World")
     data = s.recv(1024)
 
-print(f"Received {data!r}")
+print(f"Received {codecs.decode(data)!r}")
