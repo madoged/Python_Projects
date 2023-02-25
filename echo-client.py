30939bc9f92adcf681951c83d3e40339474c5043
Matthew Doged <madoged@gmail.com>
2023-02-25 07:08:14 -0500

    add mac.txt. to create/write

--- echo-client.py
+++ echo-client.py
@@ -13,11 +13,16 @@ args = parser.parse_args()
 HOST = args.Host
 PORT = 65432
 
-print(f"This is the value contained in Host {HOST}")
+f = open("mymac.txt", "w")
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST,PORT))
     s.sendall(b"Hello, World")
     data = s.recv(1024)
+    
+
 
 print(f"Received {codecs.decode(data)!r}")
+mac = str(data)
+f.write(mac)
+f.close()
